491583fa8456acc54b5d9c28332c26a1d8db9acb
shaojunying <shaojunying1@gmail.com>
2023-04-27 20:20:07 +0800

    Placeholder

---
+++ mallcloud-eureka-server/README.md
@@ -0,0 +1,5 @@
+# mallcloud-eureka-server
+
+## 可以优化的点
+
+-[ ] 考虑怎么才能实现服务的高可用。
---
+++ mallcloud-eureka-server/src/test/java/com/shao/mallcloud/mallcloudeurekaserver/MallcloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shao.mallcloud.mallcloudeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallcloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
